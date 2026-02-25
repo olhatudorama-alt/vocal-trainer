cda4c312b21c667ff0d254a50d736ef8487d5597
olhatudorama-alt <olhatudorama@gmail.com>
2026-02-25 20:56:11 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'vocal-trainer-v1';
+const ASSETS = [
+  '/vocal-trainer/',
+  '/vocal-trainer/index.html',
+  '/vocal-trainer/manifest.json',
+  '/vocal-trainer/icon-192.png',
+  '/vocal-trainer/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
